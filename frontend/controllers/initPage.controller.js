80caca9fdef5ca8bde7155ddad24e7606e2bcc7b
Romeu Neves <51343870+rnevesphp@users.noreply.github.com>
2024-01-18 15:20:57 +0100

    :package: connecting the js docs

---
+++ frontend/controllers/initPage.controller.js
@@ -0,0 +1,9 @@
+
+export function init() { 
+    console.log('HOLA')
+    const doc = document; 
+    const principalContent = doc.getElementById('main'); 
+
+    
+}
+
--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,10 @@
 // primeiro vamos criar uma variavel para manipular o DOM 
-var doc = document ; 
+import { init } from "./controllers/initPage.controller.js";
 
+let web ; 
+
+window.onload = function () {
+    if (document.readyState == "complete") {
+        web = new init();
+    }
+}
